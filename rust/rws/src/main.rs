ae2948b8bb1414c879867fcf3c2e389882961c15
Richard Cook <rcook@rcook.org>
2020-03-04 22:37:04 -0800

    Provide --fail-fast and --no-fail-fast options for run and git commands

--- rust/rws/src/main.rs
+++ rust/rws/src/main.rs
@@ -20,6 +20,46 @@ const GIT_SUBCOMMAND: &str = "git";
 const INFO_SUBCOMMAND: &str = "info";
 const RUN_SUBCOMMAND: &str = "run";
 
+struct BoolSwitch<'a> {
+    name: &'a str,
+    help: &'a str,
+    no_name: &'a str,
+    no_help: &'a str,
+}
+
+impl<'a> BoolSwitch<'a> {
+    fn new(name: &'a str, help: &'a str, no_name: &'a str, no_help: &'a str) -> BoolSwitch<'a> {
+        BoolSwitch {
+            name: name,
+            help: help,
+            no_name: no_name,
+            no_help: no_help,
+        }
+    }
+}
+
+trait BoolSwitchExt<'a> {
+    fn bool_switch(self, bs: BoolSwitch<'a>) -> Self;
+}
+
+impl<'a, 'b> BoolSwitchExt<'a> for App<'a, 'b> {
+    fn bool_switch(self, bs: BoolSwitch<'a>) -> Self {
+        self.arg(
+            Arg::with_name(bs.name)
+                .long(bs.name)
+                .help(bs.help)
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name(bs.no_name)
+                .conflicts_with(bs.name)
+                .long(bs.no_name)
+                .help(bs.no_help)
+                .takes_value(false),
+        )
+    }
+}
+
 fn main() -> std::io::Result<()> {
     #[cfg(windows)]
     set_virtual_terminal(true).unwrap();
@@ -33,6 +73,12 @@ fn main() -> std::io::Result<()> {
         .subcommand(
             SubCommand::with_name(GIT_SUBCOMMAND)
                 .about("Run Git command in each project directory")
+                .bool_switch(BoolSwitch::new(
+                    "fail-fast",
+                    "Aborts command on first error (default)",
+                    "no-fail-fast",
+                    "Runs command in all project directories",
+                ))
                 .arg(
                     Arg::with_name("cmd")
                         .help("Command to pass to Git")
@@ -43,6 +89,12 @@ fn main() -> std::io::Result<()> {
         .subcommand(
             SubCommand::with_name(RUN_SUBCOMMAND)
                 .about("Run command in each project directory")
+                .bool_switch(BoolSwitch::new(
+                    "fail-fast",
+                    "Aborts command on first error (default)",
+                    "no-fail-fast",
+                    "Runs command in all project directories",
+                ))
                 .arg(
                     Arg::with_name("cmd")
                         .help("Command to pass to shell")
@@ -55,6 +107,7 @@ fn main() -> std::io::Result<()> {
         (GIT_SUBCOMMAND, Some(_)) => {
             let submatches = matches.subcommand_matches(GIT_SUBCOMMAND).unwrap();
             do_git(
+                !submatches.is_present("no-fail-fast"),
                 &submatches
                     .values_of("cmd")
                     .map(|x| x.collect())
@@ -65,6 +118,7 @@ fn main() -> std::io::Result<()> {
         (RUN_SUBCOMMAND, Some(_)) => {
             let submatches = matches.subcommand_matches(RUN_SUBCOMMAND).unwrap();
             do_run(
+                !submatches.is_present("no-fail-fast"),
                 &submatches
                     .values_of("cmd")
                     .map(|x| x.collect())
@@ -75,12 +129,12 @@ fn main() -> std::io::Result<()> {
     }
 }
 
-fn do_git(cmd: &Vec<&str>) -> std::io::Result<()> {
+fn do_git(fail_fast: bool, cmd: &Vec<&str>) -> std::io::Result<()> {
     if cmd.len() < 1 {
         panic!("Unimplemented");
     }
 
-    run_helper(|| {
+    run_helper(fail_fast, || {
         let mut command = Command::new("git");
         for i in 0..(cmd.len()) {
             command.arg(&cmd[i]);
@@ -116,12 +170,12 @@ fn show_project_dirs(order: &str, project_dirs: &Vec<PathBuf>) {
     }
 }
 
-fn do_run(cmd: &Vec<&str>) -> std::io::Result<()> {
+fn do_run(fail_fast: bool, cmd: &Vec<&str>) -> std::io::Result<()> {
     if cmd.len() < 1 {
         panic!("Unimplemented");
     }
 
-    run_helper(|| {
+    run_helper(fail_fast, || {
         let mut command = Command::new(&cmd[0]);
         for i in 1..(cmd.len()) {
             command.arg(&cmd[i]);
@@ -130,16 +184,44 @@ fn do_run(cmd: &Vec<&str>) -> std::io::Result<()> {
     })
 }
 
-fn run_helper<F>(f: F) -> std::io::Result<()>
+fn run_helper<F>(fail_fast: bool, f: F) -> std::io::Result<()>
 where
     F: Fn() -> Command,
 {
     let current_dir = env::current_dir()?;
     let workspace = Workspace::find(&current_dir).unwrap();
+    let mut failure_count = 0;
     for project_dir in &workspace.project_dirs_topo {
-        println!("  {}", project_dir.to_str().unwrap().cyan());
-        let exit_status = f().current_dir(&project_dir).spawn().unwrap().wait();
-        println!("exit_status={:?}", exit_status)
+        let d = project_dir.to_str().unwrap();
+        println!("{}", d.cyan());
+        let exit_status = f()
+            .current_dir(&project_dir)
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+        if exit_status.success() {
+            println!("{}", format!("Command succeeded in {}", d).green())
+        } else {
+            failure_count += 1;
+            match exit_status.code() {
+                Some(code) => {
+                    let m = format!("Command exited with status {} in {}", code, d);
+                    println!("{}", if fail_fast { m.red() } else { m.yellow() });
+                    if fail_fast {
+                        break;
+                    }
+                }
+                None => println!("{}", format!("Command terminated by signal in {}", d).red()),
+            }
+        }
+    }
+
+    if !fail_fast && failure_count > 0 {
+        println!(
+            "{}",
+            format!("Command failed in {} project directories", failure_count).red()
+        )
     }
 
     Ok(())
