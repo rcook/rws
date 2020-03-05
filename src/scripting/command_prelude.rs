12159d6a48b7c9434e0fcbe6a8fb8f7efade89bd
Richard Cook <rcook@rcook.org>
2020-03-05 00:11:13 -0800

    Alphabetize prelude functions

--- src/scripting/command_prelude.rs
+++ src/scripting/command_prelude.rs
@@ -1,14 +1,14 @@
 use rlua::prelude::LuaResult;
 use std::path::Path;
 
-pub fn is_file(path: String) -> LuaResult<bool> {
-    Ok(Path::new(&path).is_file())
-}
-
 pub fn current_dir() -> LuaResult<String> {
     Ok(std::env::current_dir()
         .unwrap()
         .to_str()
         .unwrap()
         .to_string())
+}
+
+pub fn is_file(path: String) -> LuaResult<bool> {
+    Ok(Path::new(&path).is_file())
 }
