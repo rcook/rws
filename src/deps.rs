58dc6302fd53f7a30c8a2e0fc271afa9dd0bf1f6
Richard Cook <rcook@rcook.org>
2020-03-06 17:10:03 -0800

    Return errors from with_working_dir

--- src/deps.rs
+++ src/deps.rs
@@ -5,7 +5,7 @@ use crate::scripting::command::Command;
 use std::path::Path;
 
 pub fn get_deps(project_dir: &Path, command: &Command) -> Result<Vec<String>> {
-    with_working_dir(project_dir, || command.eval())?
+    with_working_dir(project_dir, || command.eval())??
         .as_str_vec()
         .ok_or_else(|| user_error("Invalid string vector"))
 }
--- src/os.rs
+++ src/os.rs
@@ -1,13 +1,13 @@
 use std::env::{current_dir, set_current_dir};
 use std::path::Path;
 
-pub fn with_working_dir<F, R>(dir: &Path, f: F) -> R
+pub fn with_working_dir<F, R>(dir: &Path, f: F) -> std::io::Result<R>
 where
     F: FnOnce() -> R,
 {
-    let saved_dir = current_dir().unwrap();
-    set_current_dir(dir).unwrap();
+    let saved_dir = current_dir()?;
+    set_current_dir(dir)?;
     let result = f();
-    set_current_dir(saved_dir).unwrap();
-    result
+    set_current_dir(saved_dir)?;
+    Ok(result)
 }
