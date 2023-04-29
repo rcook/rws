94d2d0809bccc6fc2e9a16e019645b816abebef9
Richard Cook <rcook@rcook.org>
2023-04-29 11:47:20 -0700

    Refactor with_working_dir

--- src/os/paths.rs
+++ src/os/paths.rs
@@ -19,19 +19,7 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //
-use std::env::{current_dir, set_current_dir};
 use std::path::{Component, Path};
-
-pub fn with_working_dir<F, R>(dir: &Path, f: F) -> std::io::Result<R>
-where
-    F: FnOnce() -> R,
-{
-    let saved_dir = current_dir()?;
-    set_current_dir(dir)?;
-    let result = f();
-    set_current_dir(saved_dir)?;
-    Ok(result)
-}
 
 pub fn path_to_str(path: &Path) -> &str {
     path.to_str()
