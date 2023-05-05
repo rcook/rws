345a0c553414ecde2ae1e629016114068f2d2484
Richard Cook <rcook@rcook.org>
2023-05-04 17:04:05 -0700

    Test conversion of YAML into Lua table

--- src/scripting/lua/lua_config.rs
+++ src/scripting/lua/lua_config.rs
@@ -62,3 +62,62 @@ fn translate_helper<'a>(lua_ctx: Context<'a>, yaml: &Yaml) -> rlua::Result<Value
         _ => unimplemented!("Unsupported YAML node type"),
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::translate_helper;
+    use anyhow::Result;
+    use rlua::prelude::Lua;
+    use yaml_rust::YamlLoader;
+
+    fn render_yaml_with_lua(yaml_text: &str) -> Result<String> {
+        let inspect_script = r#"
+function inspect(obj)
+    if type(obj) == "table" then
+        local s = "{ "
+        local idx = 0
+        for key, value in pairs(obj) do
+            if idx > 0 then
+                s = s .. ", "
+            end
+
+            if not (type(k) ~= "number") then
+                key = "\"" .. key .. "\""
+            end
+
+            s = s .. "[" .. key .. "] = " .. inspect(value)
+
+            idx = idx + 1
+        end
+        return s .. " }"
+    else
+        return tostring(obj)
+    end
+end
+
+return inspect(INPUT)
+"#;
+
+        let mut docs = YamlLoader::load_from_str(yaml_text)?;
+        assert_eq!(1, docs.len());
+        let yaml = docs.remove(0);
+        Lua::new().context(|lua_ctx| -> Result<String> {
+            let value = translate_helper(lua_ctx, &yaml)?;
+            lua_ctx.globals().set("INPUT", value)?;
+            Ok(lua_ctx.load(inspect_script).eval()?)
+        })
+    }
+
+    #[test]
+    fn translate_helper_basics() -> Result<()> {
+        assert_eq!(
+            "{ [1] = one, [2] = two }",
+            render_yaml_with_lua(
+                r#"- one
+- two
+"#
+            )?
+        );
+        Ok(())
+    }
+}
